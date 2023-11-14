d9fe1af22caf73ff973cedcde55f871a5a856371
priya-prasad-dev <priyaprasad.pd@gmail.com>
2023-11-14 23:20:56 +0530

    home-css

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,7 @@
-import Courousel from "../components/mainpage/Courousel";
 import { Link } from "react-router-dom";
+import Courousel from "../components/mainpage/Courousel";
 // import ConditionCard from "../../../components/conditionCard";
-import ConditionCard from "../components/conditionCard";
+import HomeCard from "../components/HomeCards";
 const Home = () => {
   return (
     <>
@@ -20,11 +20,12 @@ const Home = () => {
               <div className="container">
                 <div className="row">
                   {cardData.map((card, index) => (
-                    <div key={index} className="col-lg-4 col-md-6 mb-4" 
-                    // style={{ aspectRatio: "1/1" }}
-
+                    <div
+                      key={index}
+                      className="col-lg-4 col-md-6 mb-4"
+                      // style={{ aspectRatio: "1/1" }}
                     >
-                      <ConditionCard {...card} />
+                      <HomeCard {...card} />
                     </div>
                   ))}
                 </div>
@@ -208,51 +209,45 @@ const cardData = [
   },
   {
     imageSrc: "/mission-walk-images/06c8b886-0988-43f5-b120-7779c48e6cf3.JPG",
+    title: "Brain Injury Recovery",
+    text: "People from all over the world seek our 70 years’ experience in treating the most complex traumatic brain injuries and illnesses. We bring you the latest and most promising treatments and therapies.",
+    linkTo: "/brain",
+  },
+  {
+    imageSrc: "/mission-walk-images/7dfa808a-797c-438d-a32b-2d549672f1c5 2.JPG",
+    title: "Multiple Sclerosis",
+    text: "People with multiple sclerosis (MS) come to us for inpatient, outpatient or day rehabilitation. We focus on medical treatments and therapies for building strength, motor function and communication. We also offer guidance on diet, support groups and assistive devices.",
+    linkTo: "/conditions/multiple-sclerosis",
+  },
+
+  {
+    imageSrc: "/mission-walk-images/547c7022-dc4d-4636-9a12-4b25c9b69de9.JPG",
     title: "Parkinson's Disease & Neurologic Rehabilitation",
     text: "With a comprehensive team of clinicians trained in treating Parkinson’s disease and movement disorders, we go beyond medication and surgery with specialized treatment including therapy, technology and scientific advances to dramatically improve abilities.",
     linkTo: "/conditions/parkinsons",
   },
-  // {
-  //   imageSrc: "/mission-walk-images/7dfa808a-797c-438d-a32b-2d549672f1c5 2.JPG",
-  //   title: "",
-  //   text: "",
-  //   linkTo: "/therapy-tools/neurosuit",
-  // },
-
-  // {
-  //   imageSrc: "/mission-walk-images/547c7022-dc4d-4636-9a12-4b25c9b69de9.JPG",
-  //   title: "",
-  //   text: "",
-  //   linkTo: "/pediatric-physiotherapy",
-  // },
-  // {
-  //   imageSrc: "/mission-walk-images/547c7022-dc4d-4636-9a12-4b25c9b69de9.JPG",
-  //   title: "",
-  //   text: "",
-  //   linkTo: "/pediatric-physiotherapy",
-  // },
-  // {
-  //   imageSrc: "/mission-walk-images/547c7022-dc4d-4636-9a12-4b25c9b69de9.JPG",
-  //   title: "",
-  //   text: "",
-  //   linkTo: "/pediatric-physiotherapy",
-  // },
-  // {
-  //   imageSrc: "/mission-walk-images/547c7022-dc4d-4636-9a12-4b25c9b69de9.JPG",
-  //   title: "",
-  //   text: "",
-  //   linkTo: "/pediatric-physiotherapy",
-  // },
-  // {
-  //   imageSrc: "/mission-walk-images/547c7022-dc4d-4636-9a12-4b25c9b69de9.JPG",
-  //   title: "Theratogs",
-  //   text: "TheraTogs are an orthotic undergarment and strapping product recommended for children who may benefit from increased proprioception as well as improved postural alignment, stability, and/or prolonged muscle stretch to improve developmental and functional outcomes.",
-  //   // linkTo: "/pediatric-physiotherapy",
-  // },
-  // {
-  //   imageSrc: "mission-walk-images/mission-walk-hyderabad-5cf92914afb61.jpeg",
-  //   title: "Paediatric Speech Therapy",
-  //   text: "Paediatric speech therapists use a variety of therapy techniques to address oral motor dysfunction, apraxia, fluency, receptive and expressive language to help your child improve overall communication and feeding development.",
-  //   linkTo: "/page1",
-  // },//css for adding 4 cards <div key={index} className="col-lg-3 col-md-6 mb-4">
+  {
+    imageSrc: "/mission-walk-images/547c7022-dc4d-4636-9a12-4b25c9b69de9.JPG",
+    title: "Cerebral Palsy",
+    text: "Each person with cerebral palsy (CP) has unique functional and therapeutic needs. We treat patients with CP of any age, at any point in their lives with the latest therapies for CP rehabilitation.",
+    linkTo: "/conditions/cerebral-palsy",
+  },
+  {
+    imageSrc: "/mission-walk-images/547c7022-dc4d-4636-9a12-4b25c9b69de9.JPG",
+    title: "Pain Management",
+    text: "We address pain in two distinct ways: Chronic pain cases for conditions like sciatic nerve pain, amputation or long-term back pain are treated at our Pain Management Center. Cases of acute or short-term pain are seen by our expert DayRehab® and outpatient clinicians.",
+    linkTo: "/conditions/pain-management",
+  },
+  {
+    imageSrc: "/mission-walk-images/547c7022-dc4d-4636-9a12-4b25c9b69de9.JPG",
+    title: "Limb Loss & Impairment",
+    text: "Amputation or limb impairment can affect ability, but doesn’ can affect ability, but doesn’t have to affect the quality of your life. We meet patients’ unique needs by integrating medical expertise, technology, engineering (prosthetics, orthotics, bionics, robotic devices) and a range of therapies.t have to affect the quality of your life. We meet patients’ unique needs by integrating medical expertise, technology, engineering (prosthetics, orthotics, bionics, robotic devices) and a range of therapies.",
+    linkTo: "/conditions/difficulty-in-walking",
+  },
+  {
+    imageSrc: "/mission-walk-images/547c7022-dc4d-4636-9a12-4b25c9b69de9.JPG",
+    title: "View All",
+    text: "We go beyond traditional physical,occupational, and speech therapy by using innovative tools and techniques that change lives.",
+    linkTo: "/service",
+  },
 ];
