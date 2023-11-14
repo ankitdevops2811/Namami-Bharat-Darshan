dc03b8452361384751e421d7e0c8c99de09819d1
Ankit Singh1 <Ankit.Singh1@kelltontech.org>
2023-11-14 11:39:21 +0530

    service added

--- src/pages/Service.jsx
+++ src/pages/Service.jsx
@@ -1,9 +1,13 @@
+import { Link } from "react-router-dom";
+import ContactPagesCard from "../components/contactPagesCard";
+
 const Service = () => {
   return (
     // <div>sefvice</div>
     <>
       {/* <!-- Service Start --> */}
-      <div className="container-fluid py-5 wow fadeInUp" data-wow-delay="0.1s">
+      <div className="container-fluid mt-5 wow fadeInUp" data-wow-delay="0.1s">
+        {/* <div className="container-fluid py-5 wow fadeInUp" data-wow-delay="0.1s"> */}
         <div className="container">
           <div className="row g-5 mb-5">
             <div
@@ -24,19 +28,19 @@ const Service = () => {
                 />
               </div>
             </div>
-            <div className="col-lg-7">
-              <div className="section-title mb-5">
-                <h5 className="position-relative d-inline-block text-primary text-uppercase">
+            <div className="col-lg-7" style={{ marginBottom: "25%" }}>
+              <div className="section-title mb-5 mt-3">
+                <h5 className="position-relative d-inline-block text-primary text-uppercase mb-3">
                   Our Programs
                 </h5>
-                <h4 className="display-5 mb-0" style={{ fontSize: "16px" }}>
+                <h4 className="display-5 " style={{ fontSize: "16px" }}>
                   We go beyond traditional physical,occupational, and speech
                   therapy by using innovative tools and techniques that change
                   lives.
                 </h4>
               </div>
               <div className="section-title mb-5">
-                <h5 className="position-relative d-inline-block text-primary text-uppercase">
+                <h5 className="position-relative d-inline-block text-primary text-uppercase mb-3">
                   Discover our Programs
                 </h5>
                 <h4 className="display-5 mb-0" style={{ fontSize: "16px" }}>
@@ -49,7 +53,7 @@ const Service = () => {
                 </h4>
               </div>
               <div className="row g-5">
-                <div
+                {/* <div
                   className="col-md-6 service-item wow zoomIn"
                   data-wow-delay="0.6s"
                 >
@@ -59,8 +63,8 @@ const Service = () => {
                   <div className="position-relative bg-light rounded-bottom text-center p-4">
                     <h5 className="m-0">Pediatric Physiotherapy</h5>
                   </div>
-                </div>
-                <div
+                </div> */}
+                {/* <div
                   className="col-md-6 service-item wow zoomIn"
                   data-wow-delay="0.9s"
                 >
@@ -70,16 +74,22 @@ const Service = () => {
                   <div className="position-relative bg-light rounded-bottom text-center p-4">
                     <h5 className="m-0">Neuro Physiotherapy</h5>
                   </div>
-                </div>
+                </div> */}
               </div>
             </div>
           </div>
-          <div className="row g-5 wow fadeInUp" data-wow-delay="0.1s">
-            <div className="col-lg-7">
+        </div>
+      </div>
+      <section className="" style={{ marginBottom: "40px" }}>
+        <div className="container-fluid wow fadeInUp" data-wow-delay="0.1s">
+          <div className="container">
+            <div className="row g-5 wow fadeInUp" data-wow-delay="0.1s">
+              {/* <div className="col-lg-7"> */}
               <div className="row g-5">
                 <div
                   className="col-md-6 service-item wow zoomIn"
                   data-wow-delay="0.3s"
+                  style={{ width: "400px" }}
                 >
                   <div className="rounded-top overflow-hidden">
                     <img
@@ -89,11 +99,50 @@ const Service = () => {
                     />
                   </div>
                   <div className="position-relative bg-light rounded-bottom text-center p-4">
-                    <h5 className="m-0">Survical Physiotherapy</h5>
+                    <Link to="/conditions/stroke-recovery">
+                      <h5 className="m-0">Stroke Recovery</h5>
+                    </Link>
                   </div>
                 </div>
                 <div
                   className="col-md-6 service-item wow zoomIn"
+                  data-wow-delay="0.3s"
+                  style={{ width: "400px" }}
+                >
+                  <div className="rounded-top overflow-hidden">
+                    <img
+                      className="img-fluid"
+                      src="img/mission-walk-hyderabad-5cf92e1761f8b.jpeg"
+                      alt=""
+                    />
+                  </div>
+                  <div className="position-relative bg-light rounded-bottom text-center p-4">
+                    <Link to="/spinal">
+                      <h5 className="m-0">Spinal Cord Injury</h5>
+                    </Link>
+                  </div>
+                </div>
+                <div
+                  className="col-md-6 service-item wow zoomIn"
+                  data-wow-delay="0.3s"
+                  style={{ width: "400px" }}
+                >
+                  <div className="rounded-top overflow-hidden">
+                    <img
+                      className="img-fluid"
+                      src="img/mission-walk-hyderabad-5cf92e1761f8b.jpeg"
+                      alt=""
+                    />
+                  </div>
+                  <div className="position-relative bg-light rounded-bottom text-center p-4">
+                    <Link to="/brain">
+                      <h5 className="m-0">Brain Injury Recovery</h5>
+                    </Link>
+                  </div>
+                </div>
+                {/* <div
+                  className="col-md-6 service-item wow zoomIn"
+                  style={{ width: "400px" }}
                   data-wow-delay="0.6s"
                 >
                   <div className="rounded-top overflow-hidden">
@@ -102,54 +151,116 @@ const Service = () => {
                   <div className="position-relative bg-light rounded-bottom text-center p-4">
                     <h5 className="m-0">Spinal Cord Injury</h5>
                   </div>
-                </div>
-                {/* priya */}
+                </div> */}
                 <div
                   className="col-md-6 service-item wow zoomIn"
-                  data-wow-delay="0.6s"
+                  data-wow-delay="0.3s"
+                  style={{ width: "400px" }}
                 >
                   <div className="rounded-top overflow-hidden">
-                    <img className="img-fluid" src="img/sddefault.jpg" alt="" />
+                    <img
+                      className="img-fluid"
+                      src="img/mission-walk-hyderabad-5cf92e1761f8b.jpeg"
+                      alt=""
+                    />
                   </div>
                   <div className="position-relative bg-light rounded-bottom text-center p-4">
-                    <h5 className="m-0">Spinal Cord Injury</h5>
+                    <Link to="/conditions/multiple-sclerosis">
+                      <h5 className="m-0">Multiple Sclerosis</h5>
+                    </Link>
+                  </div>
+                </div>
+                <div
+                  className="col-md-6 service-item wow zoomIn"
+                  data-wow-delay="0.3s"
+                  style={{ width: "400px" }}
+                >
+                  <div className="rounded-top overflow-hidden">
+                    <img
+                      className="img-fluid"
+                      src="img/mission-walk-hyderabad-5cf92e1761f8b.jpeg"
+                      alt=""
+                    />
+                  </div>
+                  <div className="position-relative bg-light rounded-bottom text-center p-4">
+                    <Link to="/conditions/parkinsons">
+                      <h5 className="m-0">Parkinson's Disease</h5>
+                    </Link>
+                  </div>
+                </div>
+                <div
+                  className="col-md-6 service-item wow zoomIn"
+                  data-wow-delay="0.3s"
+                  style={{ width: "400px" }}
+                >
+                  <div className="rounded-top overflow-hidden">
+                    <img
+                      className="img-fluid"
+                      src="img/mission-walk-hyderabad-5cf92e1761f8b.jpeg"
+                      alt=""
+                    />
+                  </div>
+                  <div className="position-relative bg-light rounded-bottom text-center p-4">
+                    <Link to="/conditions/transverse-myelitis">
+                      <h5 className="m-0">Transverse Myelitis</h5>
+                    </Link>
+                  </div>
+                </div>
+                <div
+                  className="col-md-6 service-item wow zoomIn"
+                  data-wow-delay="0.3s"
+                  style={{ width: "400px" }}
+                >
+                  <div className="rounded-top overflow-hidden">
+                    <img
+                      className="img-fluid"
+                      src="img/mission-walk-hyderabad-5cf92e1761f8b.jpeg"
+                      alt=""
+                    />
+                  </div>
+                  <div className="position-relative bg-light rounded-bottom text-center p-4">
+                    <Link to="/conditions/motor-neuron">
+                      <h5 className="m-0">Motor Neuron</h5>
+                    </Link>
+                  </div>
+                </div>
+                <div
+                  className="col-md-6 service-item wow zoomIn"
+                  data-wow-delay="0.3s"
+                  style={{ width: "400px" }}
+                >
+                  <div className="rounded-top overflow-hidden">
+                    <img
+                      className="img-fluid"
+                      src="img/mission-walk-hyderabad-5cf92e1761f8b.jpeg"
+                      alt=""
+                    />
+                  </div>
+                  <div className="position-relative bg-light rounded-bottom text-center p-4">
+                    <Link to="/conditions/difficulty-in-walking">
+                      <h5 className="m-0">Difficulty InWalking</h5>
+                    </Link>
+                  </div>
+                </div>
+                <div
+                  className="col-md-6 service-item wow zoomIn"
+                  data-wow-delay="0.3s"
+                  style={{ width: "400px" }}
+                >
+                  <div className="position-relative bg-primary rounded h-100 d-flex flex-column align-items-center justify-content-center text-center p-4">
+                    <h3 className="text-white mb-3">Make Appointment</h3>
+                    <p className="text-white mb-3">
+                      Please Reach out to Mission-Walk Healpline Number
+                    </p>
+                    <h2 className="text-white mb-0">+91 9177300194</h2>
                   </div>
                 </div>
               </div>
             </div>
-          
           </div>
         </div>
-      </div>
-{/* pppp */}
-      {/* <section>
-        <div
-          className="container-fluid py-5 wow fadeInUp"
-          data-wow-delay="0.1s"
-        >
-          <div className="container">
-            <div className="row g-5 mb-5">
-              <div
-                className="col-lg-5 wow zoomIn"
-                data-wow-delay="0.3s"
-                style={{ minHeight: "400px" }}
-              >
-                <div
-                  className="col-md-6 service-item wow zoomIn"
-                  data-wow-delay="0.6s"
-                >
-                  <div className="rounded-top overflow-hidden">
-                    <img className="img-fluid" src="img/sddefault.jpg" alt="" />
-                  </div>
-                  <div className="position-relative bg-light rounded-bottom text-center p-4">
-                    <h5 className="m-0">Spinal Cord Injury</h5>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </section> */}
+      </section>
+      <ContactPagesCard />
       {/* <!-- Service End --> */}
     </>
   );
